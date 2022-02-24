a9395ae1d3961669e01c2c5e649a71edc57caf05
Nishat Shahrin <72220615+nishatcse25@users.noreply.github.com>
2022-02-25 00:10:13 +0600

    Add files via upload

---
+++ SCAN-d.cpp
@@ -0,0 +1,112 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int size ;
+int disk_size = 200;
+
+void SCAN(int arr[], int head, string direction)
+{
+    int headposition=head;
+    int seek_count = 0;
+    int distance, cur_track;
+    vector<int> left, right;
+    vector<int> seek_sequence;
+
+    if (direction == "left")
+        left.push_back(0);
+    else if (direction == "right")
+        right.push_back(disk_size - 1);
+
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] < head)
+            left.push_back(arr[i]);
+        if (arr[i] > head)
+            right.push_back(arr[i]);
+    }
+
+
+    std::sort(left.begin(), left.end());
+    std::sort(right.begin(), right.end());
+
+    int run = 2;
+    while (run--)
+    {
+        if (direction == "left")
+        {
+            for (int i = left.size() - 1; i >= 0; i--)
+            {
+                cur_track = left[i];
+
+                seek_sequence.push_back(cur_track);
+
+                distance = abs(cur_track - head);
+                    if(head>cur_track){
+                        printf("(%d - %d)",head,cur_track);
+
+                    }else
+                     printf("(%d - %d)",cur_track,head);
+
+                seek_count += distance;
+
+                head = cur_track;
+            }
+            direction = "right";
+        }
+        else if (direction == "right")
+        {
+            for (int i = 0; i < right.size(); i++)
+            {
+                cur_track = right[i];
+                seek_sequence.push_back(cur_track);
+
+                distance = abs(cur_track - head);
+                if(head>cur_track){
+                        printf("(%d - %d)",head,cur_track);
+
+                    }else
+                     printf("(%d - %d)",cur_track,head);
+
+
+                seek_count += distance;
+
+
+                head = cur_track;
+            }
+            direction = "left";
+        }
+    }
+    printf("\n");
+
+    cout << "Total number of seek operations = "
+         << seek_count << endl;
+
+    cout << "Seek Sequence is : " ;
+    cout<<headposition<<" ";
+
+    for (int i = 0; i < seek_sequence.size(); i++)
+    {
+        cout << seek_sequence[i] << " " ;
+    }
+}
+
+
+int main()
+{
+
+
+    int arr[20];
+    int head =0;
+    string direction = "left";
+     printf("Enter the number of Requests\n");
+    scanf("%d",&size);
+    printf("Enter the Requests sequence\n");
+    for(int i=0;i<size;i++)
+     scanf("%d",&arr[i]);
+    printf("Enter initial head position\n");
+    scanf("%d",&head);
+
+    SCAN(arr, head, direction);
+
+    return 0;
+}
